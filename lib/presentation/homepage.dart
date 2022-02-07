24baedcbc332bf6b5a4d90edbab4c6c3c8d5a4af
Pawan <pawan.singh.co19@nsut.ac.in>
2022-02-08 00:54:22 +0530

    first commit

---
+++ lib/presentation/homepage.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+
+
+
+class HomePage extends StatefulWidget {
+  //const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(child: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                "heading",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ],
+        ),)
+      ),
+    );
+     
+  }
+}
